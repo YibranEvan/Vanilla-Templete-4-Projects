4ede61e20f5d484adcb094ac7d70c4fa820871cb
Rodrigo <rpv6423@gmail.com>
2022-05-25 17:53:21 -0700

    052522

--- module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/03_Expressions/exercise-student/java/src/main/java/com/techelevator/Exercises.java
@@ -183,7 +183,13 @@ public class Exercises {
 	 loneTeen(13, 13) → false
 	 */
 	public boolean loneTeen(int a, int b) {
-		return false;
+		if ((a >= 13 && a <= 19) && !(b >=13 && b <= 19)) {
+			return true;
+		} else if (!(a >= 13 && a <= 19) && (b >=13 && b <= 19)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	/*
