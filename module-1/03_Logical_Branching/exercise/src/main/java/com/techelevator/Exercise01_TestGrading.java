57407dcf4a6a622c3e5a22db117e85b7d63dd25e
Rodrigo <rpv6423@gmail.com>
2022-05-27 23:38:34 -0700

    052722

--- module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise01_TestGrading.java
+++ module-1/03_Logical_Branching/exercise/src/main/java/com/techelevator/Exercise01_TestGrading.java
@@ -18,7 +18,7 @@ public class Exercise01_TestGrading {
     gradeTestPassFail(45) ➔ false
      */
     public boolean gradeTestPassFail(int score) {
-        return false;
+        return (score >= 70);
     }
 
     /*
@@ -37,7 +37,15 @@ public class Exercise01_TestGrading {
     gradeTestNumeric(10) ➔ 0
      */
     public int gradeTestNumeric(int score) {
-        return 0;
+        int grade = 0;
+        if (score >= 90) {
+            grade = 3;
+        } else if (score >= 50) {
+            grade = 2;
+        } else if (score >= 25) {
+            grade = 1;
+        }
+        return grade;
     }
 
     /*
@@ -56,6 +64,19 @@ public class Exercise01_TestGrading {
     gradeTestLetter(45) ➔ 'F'
      */
     public char gradeTestLetter(int score) {
-        return ' ';
+        char grade = 'F';
+
+        if (score >= 90) {
+            grade = 'A';
+        } else if (score >= 80) {
+            grade = 'B';
+        } else if (score >= 70) {
+            grade = 'C';
+        } else if (score >= 60) {
+            grade = 'D';
+        }
+
+        return grade;
+
     }
 }
