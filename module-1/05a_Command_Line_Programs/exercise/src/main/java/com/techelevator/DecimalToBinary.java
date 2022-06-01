fff24d83e6ab8062c0dd5cd368592233086d40ff
Rodrigo <rpv6423@gmail.com>
2022-06-01 16:13:52 -0700

    060122

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,8 +1,10 @@
 package com.techelevator;
+import java.util.Scanner;
 
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
 
 	}
 
--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -6,9 +6,32 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 
+/// start your loop on 1 instead on 0
+		Scanner userInput = new Scanner(System.in);
 
-		Scanner input = new Scanner(System.in);
-		System.out.print("Please enter an integer: ");
+		System.out.print("Please enter an number: ");
+		String numberInput = userInput.nextLine();
+		int number = Integer.parseInt(numberInput);
+
+		int a = 0, b = 1;
+
+		for (int i = 1; i <= number; i++) {
+
+
+			System.out.print(a + ", ");
+			int nextNumber = a + b;
+			a = b;
+			b = nextNumber;
+
+			if (a > number) {
+				break;
+
+			}
+		}
+	}
+}
+
+
 
 
 
