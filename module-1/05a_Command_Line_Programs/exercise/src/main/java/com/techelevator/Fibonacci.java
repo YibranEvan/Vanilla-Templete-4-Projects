1eca9f83083e106c5220c072fdbfa4249390398e
Rodrigo <rpv6423@gmail.com>
2022-05-31 22:31:36 -0700

    053122

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -6,13 +6,35 @@ public class Fibonacci {
 
 	public static void main(String[] args) {
 
-		Scanner userInput = new Scanner(System.in);
-		System.out.print("Please enter a number: ");
-		String fibonacciInput = userInput.nextLine();
-		int fibonacci = Integer.parseInt(fibonacciInput);
+	//	Scanner userInput = new Scanner(System.in);
+	//	System.out.print("Please enter a number: ");
+	//	String fibonacciInput = userInput.nextLine();
+	//	int fibonacci = Integer.parseInt(fibonacciInput);
 		//if ()
+		Scanner input = new Scanner(System.in);
+		System.out.print("Please enter an integer: ");
 
 
+
+
+
+
+		int initial = 0;
+		int addInitial = 1;
+		int keepInitial = 0;
+
+		int topNumber = input.nextInt();
+		input.nextLine();
+
+		System.out.print(0 + " ");
+
+		for (int nextNum = 1; nextNum <= topNumber;) {
+
+			System.out.print(nextNum + " ");
+
+			keepInitial = initial;
+			initial = nextNum;
+			nextNum= nextNum + keepInitial;
+		}
 	}
-
 }
