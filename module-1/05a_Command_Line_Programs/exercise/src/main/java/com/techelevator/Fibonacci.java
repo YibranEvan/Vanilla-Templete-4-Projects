d52bac9a8463f673a34b43f78babe400196eadac
Rodrigo <rpv6423@gmail.com>
2022-05-31 21:04:08 -0700

    -053122

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/Fibonacci.java
@@ -1,8 +1,17 @@
 package com.techelevator;
+import java.util.Scanner;
+
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
+
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("Please enter a number: ");
+		String fibonacciInput = userInput.nextLine();
+		int fibonacci = Integer.parseInt(fibonacciInput);
+		//if ()
+
 
 	}
 
