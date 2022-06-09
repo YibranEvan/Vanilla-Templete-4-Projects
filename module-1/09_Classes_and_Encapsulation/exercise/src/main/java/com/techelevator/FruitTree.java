8f96fde910cb97d8983be283989c1634cffbc648
Rodrigo <rpv6423@gmail.com>
2022-06-09 15:05:54 -0700

    060922

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/FruitTree.java
@@ -4,6 +4,11 @@ public class FruitTree {
     private String typeOfFruit;
     private int piecesOfFruitLeft;
 
+    public FruitTree(String typeOfFruit, int piecesOfFruitLeft) {
+        this.typeOfFruit = typeOfFruit;
+        this.piecesOfFruitLeft = piecesOfFruitLeft;
+    }
+
     // Setters and getters
     public String getTypeOfFruit() {
         return typeOfFruit;
@@ -11,11 +16,13 @@ public class FruitTree {
     public int getPiecesOfFruitLeft() {
         return piecesOfFruitLeft;
     }
-
-    // Constructor
-    public FruitTree(String typeOfFruit, int piecesOfFruitLeft) {
-        this.typeOfFruit = typeOfFruit;
-        this.piecesOfFruitLeft = piecesOfFruitLeft;
+    public boolean pickFruit(int numberOfPiecesToRemove) {
+        if (piecesOfFruitLeft >= numberOfPiecesToRemove) {
+            piecesOfFruitLeft -= numberOfPiecesToRemove;
+            return true;
+        } else {
+            return false;
+        }
     }
 
-}
+    }
