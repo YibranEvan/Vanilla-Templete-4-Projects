e98d19270861f6ea0ffe36c8c073faedf4a2f1a1
Rodrigo <rpv6423@gmail.com>
2022-06-10 14:05:00 -0700

    061022

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Airplane.java
@@ -7,44 +7,62 @@ public class Airplane {
     private int totalCoachSeats;
     private int bookedCoachSeats;
 
-    public Airplane (String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
+    public Airplane(String planeNumber, int totalFirstClassSeats, int totalCoachSeats) {
         this.planeNumber = planeNumber;
         this.totalFirstClassSeats = totalFirstClassSeats;
         this.totalCoachSeats = totalCoachSeats;
     }
+
     //Setters and getters
     public String getPlaneNumber() {
         return planeNumber;
     }
+
     public int getTotalFirstClassSeats() {
         return totalFirstClassSeats;
     }
+
     public int getBookedFirstClassSeats() {
         return bookedFirstClassSeats;
     }
-    public int getAvailableFirstClassSeats() {
-        return bookedFirstClassSeats - totalFirstClassSeats;
-    }
+
+
     public int getTotalCoachSeats() {
         return totalCoachSeats;
     }
+
     public int getBookedCoachSeats() {
         return bookedCoachSeats;
     }
+
+    public int getAvailableFirstClassSeats() {
+        return totalFirstClassSeats - bookedFirstClassSeats;
+    }
+
     public int getAvailableCoachSeats() {
-        return bookedCoachSeats - totalCoachSeats ;
+        return totalCoachSeats - bookedCoachSeats;
     }
+
     public boolean reserveSeats(boolean forFirstClass, int totalNumberOfSeats) {
-        if (forFirstClass) {
-            bookedFirstClassSeats = bookedFirstClassSeats + totalNumberOfSeats;
-        } else if (!forFirstClass) {
-            bookedCoachSeats = bookedCoachSeats + totalNumberOfSeats;
+        if (forFirstClass == true) {
+            if (totalFirstClassSeats - bookedFirstClassSeats >= totalNumberOfSeats) {
+                bookedFirstClassSeats += totalNumberOfSeats;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (totalCoachSeats - bookedCoachSeats >= totalNumberOfSeats) {
+                bookedCoachSeats += totalNumberOfSeats;
+                return true;
+            } else {
+                return false;
+            }
         }
-        return forFirstClass;
     }
-
-
-
-
-
 }
+
+
+
+
+
