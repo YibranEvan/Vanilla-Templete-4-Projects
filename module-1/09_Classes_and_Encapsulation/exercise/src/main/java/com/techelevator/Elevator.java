b6decd711a6f5d5b43bef4989118a93c9607dad9
Rodrigo <rpv6423@gmail.com>
2022-06-10 13:39:27 -0700

    061022

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Elevator.java
@@ -1,25 +1,49 @@
 package com.techelevator;
 
 public class Elevator {
-    private int currentFloor;
+    private int currentFloor = 1;
     private int numberOfFloors;
     private boolean doorOpen;
-    String numberOfLevels;
 
 
-   /// public Elevator (int numberOfLevels) {
-     //   this.numberOfLevels = numberOfLevels;
-   // }
+    public Elevator(int numberOfFloors) {
+        this.numberOfFloors = numberOfFloors;
+    }
 
     //getters and setters
     public int getCurrentFloor() {
         return currentFloor;
     }
+
     public int getNumberOfFloors() {
         return numberOfFloors;
     }
+
     public boolean isDoorOpen() {
         return doorOpen;
     }
-//public Elevator (int  {
+
+    // Methods
+    public void openDoor() {
+        doorOpen = true;
+    }
+
+    public void closeDoor() {
+        doorOpen = false;
+
+    }
+
+    public void goUp(int desiredFloor) {
+        if (!doorOpen && desiredFloor > currentFloor && desiredFloor <= numberOfFloors) {
+            currentFloor = desiredFloor;
+        }
+    }
+
+    public void goDown(int desiredFloor) {
+        if (!doorOpen && desiredFloor < currentFloor && desiredFloor >= 0) {
+            currentFloor = desiredFloor;
+
+        }
+
+    }
 }
