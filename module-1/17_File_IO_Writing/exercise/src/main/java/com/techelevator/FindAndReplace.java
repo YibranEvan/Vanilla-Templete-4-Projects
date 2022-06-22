fcfa91612b1d6a474f75d8e832bd52ed4eb2a22e
Rodrigo <rpv6423@gmail.com>
2022-06-22 13:28:09 -0700

    062222

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,36 @@
 package com.techelevator;
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.PrintWriter;
 public class FindAndReplace {
 
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.print("What is the replacement word?");
+        String searchWord = userInput.nextLine();
+
+        System.out.print("What is the search word you are looking for?");
+        String wordReplace = userInput.nextLine();
+
+        System.out.print("What is the source file?");
+        String srcFile = userInput.nextLine();
+
+        System.out.print("What is the destination file?");
+        String fileDes = userInput.nextLine();
+
+        File sourceFile = new File(srcFile);
+        File fileDestination = new File(fileDes);
+
+
+        try (Scanner fileInput = new Scanner(sourceFile.getAbsoluteFile()); PrintWriter dataOutput = new PrintWriter(fileDestination.getAbsoluteFile())) {
+            while (fileInput.hasNextLine()) {
+                String lineOfInput = fileInput.nextLine();
+                dataOutput.println(lineOfInput.replaceAll(searchWord, wordReplace));
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("The file " + srcFile + "Was not found, please try again");
+        }
     }
-
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,8 +1,16 @@
 package com.techelevator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 
 public class FizzWriter {
 
 	public static void main(String[] args) {
+
+
+
+
 
 	}
 
