7b567c53f43de66aa72370fc7c585ba919a4c9df
Rodrigo <rpv6423@gmail.com>
2022-06-26 15:26:55 -0700

    062622

--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/18_city_count.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/18_city_count.sql
@@ -1,5 +1,10 @@
 -- 18. The count of the number of cities (name column 'num_cities') and the state abbreviation for each state and territory (exclude state abbreviation DC).
 -- Order the results by state abbreviation.
 -- (55 rows)
-SELECT COUNT() AS num_cities
+SELECT COUNT(city_id) AS num_cities, state_abbreviation
+FROM city 
+WHERE state_abbreviation NOT IN('DC')
+GROUP BY state_abbreviation
+ORDER BY state_abbreviation 
+
 
--- module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
+++ module-2/02_Ordering_Limiting_and_Grouping/exercise/Exercises/20_largest_city_per_state.sql
@@ -1,4 +1,7 @@
 -- 20. The state abbreviation, and population of the largest city (name column 'city_population') of all states, territories, and districts.
 -- Order the results from highest to lowest populations.
 -- (56 rows)
-
+SELECT state_abbreviation, population AS city_population
+FROM state
+WHERE state_abbreviation IS NOT NULL
+ORDER BY city_population DESC
