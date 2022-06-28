47c39a0d1081aa4523efad3459e1c7d3975a89ef
Rodrigo <rpv6423@gmail.com>
2022-06-27 19:44:51 -0700

    062722

--- module-2/03_Joins/exercise/Exercises/02_name_birthday_fifth_element.sql
+++ module-2/03_Joins/exercise/Exercises/02_name_birthday_fifth_element.sql
@@ -1,3 +1,6 @@
 -- 2. The names and birthdays of actors in "The Fifth Element" (15 rows)
-
+SELECT person_name, birthday
+FROM person
+JOIN movie_actor ON actor_id = person_id
+WHERE movie_id = (SELECT movie_id FROM movie WHERE title = 'The Fifth Element');
 
