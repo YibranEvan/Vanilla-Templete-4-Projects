07ed59c8f478395818029fae1a0908589b779621
Rodrigo <rpv6423@gmail.com>
2022-06-28 14:42:05 -0700

    062822

--- module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
+++ module-2/03_Joins/exercise/Exercises/15_actor_and_director.sql
@@ -1,2 +1,6 @@
 -- 15. The title of the movie and the name of director for movies where the director was also an actor in the same movie (73 rows)
-
+SELECT DISTINCT title, person_name
+FROM person 
+JOIN movie_actor ON movie_actor.actor_id = person.person_id
+JOIN movie ON movie_actor.movie_id = movie.movie_id
+WHERE director_id = actor_id
