b6f9aa4f931ad4c1570918ace40c7bb832cf661c
Rodrigo <rpv6423@gmail.com>
2022-07-18 16:17:20 -0700

    071822

--- module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatFactService.java
+++ module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatFactService.java
@@ -2,19 +2,28 @@ package com.techelevator.services;
 
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
+
+
+
 import com.techelevator.model.CatFact;
 
 @Component
 public class RestCatFactService implements CatFactService {
-
+	public static final String API_BASE_URL = "https://cat-data.netlify.app/api/facts/random";
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Override
-	public CatFact getFact() {
-		// use getForObject method here
-		// first param: url
-		// second param: CatFact.class
-		// TODO Auto-generated method stub
-		return null;
+	public CatFact getFact() throws RestClientResponseException {
+		CatFact fact = restTemplate.getForObject(API_BASE_URL, CatFact.class);
+		return fact;
 	}
 
 }
+	// use getForObject method here
+		// first param: url
+		// second param: CatFact.class
+		// TODO Auto-generated method stub
+
+
--- module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatPicService.java
+++ module-2/Cat_Card_Capstone/exercise/src/main/java/com/techelevator/services/RestCatPicService.java
@@ -1,19 +1,24 @@
 package com.techelevator.services;
 
+import com.techelevator.model.CatFact;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatPicService implements CatPicService {
+	public static final String API_BASE_URL = "https://cat-data.netlify.app/api/pictures/random";
+	private RestTemplate restTemplate = new RestTemplate();
 
 	@Override
-	public CatPic getPic() {
-		// use getForObject method here
-		// first param: url
-		// second param: CatPic.class
-		// TODO Auto-generated method stub
-		return null;
+	public CatPic getPic() throws RestClientResponseException {
+		CatPic pic = restTemplate.getForObject(API_BASE_URL, CatPic.class);
+		return pic;
 	}
-
-}	
+}
+// use getForObject method here
+// first param: url
+// second param: CatPic.class
+// TODO Auto-generated method stub
