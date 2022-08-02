8c1e0dd531d4d511e6033681cde1dd2757a0e0ef
Rodrigo <rpv6423@gmail.com>
2022-08-02 16:03:04 -0700

    080222

--- module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <form v-on:submit.prevent="addNewBook" >
-      <div class=".new-book-form">
-        <label for=".title-input">Title:</label>
-        <input id=".title-input" type="text" v-model="newBook.title" />
+      <div class="new-book-form">
+        <label for="title-input">Title:</label>
+        <input class="title-input" type="text" v-model="newBook.title" />
       </div>
-      <div class=".new-book-form">
-        <label for=".author-input">Author:</label>
-        <input id=".author-input" type="text" v-model="newBook.author" />
+      <div class="new-book-form">
+        <label for="author-input">Author:</label>
+        <input class="author-input" type="text" v-model="newBook.author" />
       </div>
-      <div class=".new-book-form">
-        <label for=".isbn-input">ISBN:</label>
-        <input id=".isbn-input" type="text" v-model="newBook.isbn" />
+      <div class="new-book-form">
+        <label for="isbn-input">ISBN:</label>
+        <input class="isbn-input" type="text" v-model="newBook.isbn" />
       </div>
       <input type="submit" value="Save" />
       <input type="button" value="Cancel" v-on:click.prevent="resetForm" />
